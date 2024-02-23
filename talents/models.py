88566d09ac30856287c15bf273a31a739731370d
= <=>
2024-02-23 12:39:33 +0900

    [update] 作成日時/更新日時を追加した

--- talents/models.py
+++ talents/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 class Talent(models.Model):
     name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'talents'
