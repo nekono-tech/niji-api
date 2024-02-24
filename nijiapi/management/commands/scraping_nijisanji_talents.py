1fdd82ae5d84b83ff378a7105b94ed1fad2d7bd1
= <=>
2024-02-24 22:17:22 +0900

    [update] ライバーの情報を取得できるデータを見つけたので、そこからライバーの基本情報を取得して作成/更新できるようにした

--- nijiapi/management/commands/scraping_nijisanji_talents.py
+++ nijiapi/management/commands/scraping_nijisanji_talents.py
@@ -1,8 +1,14 @@
+import json
+
 from django.core.management.base import BaseCommand
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+
+from talents.models import Talent
+
 
 class Command(BaseCommand):
     help = 'にじさんじのタレント一覧をスクレイピングして取得する'
@@ -19,5 +25,60 @@ class Command(BaseCommand):
         service = Service(executable_path=new_driver)
 
         driver = webdriver.Chrome(options=chrome_options, service=service)
-        driver.get('https://www.nijisanji.jp/talents?filter=nijisanji&orderKey=debut_at&order=asc')
-        print(driver.title)
+        url = 'https://www.nijisanji.jp/talents/'
+        driver.get(url)
+
+        # __NEXT_DATA__ に含まれる JSON を取得
+        next_data = driver.find_element(By.ID, '__NEXT_DATA__').get_attribute('innerHTML')
+        data = json.loads(next_data)
+        all_livers = data['props']['pageProps']['allLivers']
+
+        """
+        all_livers は 2024/02 時点で以下のような構造になっているので、slug が分かれば個別のタレントページにアクセスできる
+        - id                : 内部IDと思われる
+        - slug              : タレントごとのページにアクセスするための識別子
+        - name              : タレント名
+        - enName            : タレント名（英語表記）
+        - profile
+          - affiliation     : 所属事務所(にじさんじ)
+          - debutat         : デビュー日時(ISO 8601形式)
+        - images
+          - head
+            - url           : タレントの画像URL
+          - width           : 画像の幅
+          - height          : 画像の高さ
+        - socialLinks
+          - fanclub         : ファンクラブURL
+          - subscribeCount  : チャンネル登録者数
+          - orderbyRuby     : 不明
+          - orderbyEnName   : 不明
+        """
+
+        # Talent モデルにデータを保存していく
+        # ただし、既に存在している場合は更新する
+        for liver in all_livers:
+            name = liver['name'] if liver.get('name') else ''
+            name_en = liver['enName'] if liver.get('enName') else ''
+            slug = liver['slug'] if liver.get('slug') else ''
+            debut_at = liver['profile'].get('debutAt', None)
+            fanclub_url = liver['socialLinks'].get('fanclub', '')
+
+            # タレントを検索し、一致する場合は更新、それ以外の場合は新規作成
+            talent, created = Talent.objects.update_or_create(
+                name=name,
+                defaults={
+                    'name_en': name_en,
+                    'slug': slug,
+                    'debut_at': debut_at,
+                    'fanclub_url': fanclub_url
+                }
+            )
+
+            if created:
+                print(f'{talent.name} を新規作成しました')
+            else:
+                print(f'{talent.name} を更新しました')
+
+            talent.save()
+
+        driver.quit()
--- talents/models.py
+++ talents/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
-
 class Talent(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default='')
+    name_en = models.CharField(max_length=100, default='')
+    slug = models.CharField(max_length=100, default='')
+    debut_at = models.DateTimeField(null=True, default=None)
+    fanclub_url = models.URLField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
