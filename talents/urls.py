66733da5bcc096ce27a6374fe630906abc779b96
= <=>
2024-02-24 19:14:47 +0900

    [update] talents はシンプルな REST API のため ModelViewSet を使用するように変更

--- talents/urls.py
+++ talents/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from .views import TalentIndexView, TalentDetailView
+from .views import TalentViewSet
+
+router = DefaultRouter()
+router.register('talents', TalentViewSet)
 
 urlpatterns = [
-    # タレント情報一覧取得API
-    path('/', TalentIndexView.as_view(), name='talent.index'),
-    # タレント情報詳細取得API
-    path('/<int:talent_id>', TalentDetailView.as_view(), name='talent.detail'),
+    path('/', include(router.urls)),
 ]
--- talents/views.py
+++ talents/views.py
@@ -1,28 +1,12 @@
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import viewsets
 
 from talents.models import Talent
 from talents.serializers import TalentSerializer
 
 
-class TalentIndexView(APIView):
+class TalentViewSet(viewsets.ModelViewSet):
     """
-    タレント情報一覧取得API
+    タレント情報ViewSet
     """
-    def get_queryset(self):
-        return Talent.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        talents = self.get_queryset()
-        return Response(data=TalentSerializer(talents, many=True).data)
-
-
-class TalentDetailView(APIView):
-    """
-    タレント情報詳細取得API
-    """
-    def get(self, request, *args, **kwargs):
-        talent_id = kwargs.get('talent_id')
-        talent = get_object_or_404(Talent, pk=talent_id)
-        return Response(data=TalentSerializer(talent).data)
+    queryset = Talent.objects.all()
+    serializer_class = TalentSerializer
